946e870a923f439fb234d3dd860772dd3775381b
kkamali <kathleenkamali@gmail.com>
2016-11-02 10:37:15 -0700

    with validation

--- survey.py
+++ survey.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
 
 @app.route("/")
 def greetings():
@@ -10,9 +11,21 @@ def greetings():
 def result():
     print "Got Post Info"
     name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
     comment = request.form['comment']
-    return render_template("result.html", name=name, location=location,language=language, comment=comment)
+    if (len(name) < 1):
+        flash("Name cannot be empty!")
+        return redirect("/")
+    elif(len(comment) < 1):
+        flash("Comment cannot be empty!")
+        return redirect("/")
+    elif(len(comment) > 120):
+        flash("Comment cannot be longer than 120 characters")
+        return redirect("/")
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        language = request.form['language']
+        comment = request.form['comment']
+        return render_template("result.html", name=name, location=location,language=language, comment=comment)
 
 app.run(debug=True)
