f9d47f8d104e0597df1a80ddf5a0c2c0c110b2b8
kkamali <kathleenkamali@gmail.com>
2016-11-01 19:43:47 -0700

    initial commit

---
+++ survey.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def greetings():
+    return render_template("index.html")
+
+@app.route("/result", methods=['POST'])
+def result():
+    print "Got Post Info"
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template("result.html", name=name, location=location,language=language, comment=comment)
+
+app.run(debug=True)
